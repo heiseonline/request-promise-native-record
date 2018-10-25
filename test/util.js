0d6b7a48af504829281ab8dd334d250b882d7c09
apl <apl@heise.de>
2018-10-25 08:29:14 +0200

    upgrade eslint modules
    closes #11
    closes #6

--- test/util.js
+++ test/util.js
@@ -4,25 +4,25 @@ const util = require('../lib/util')
 describe('Util', () => {
   it('#stringify', () => {
     const expected = '{"a":2,"x":{"a":2,"z":1},"z":1}'
-    assert.equal(util.stringify({ z: 1, a: 2, x: { z: 1, a: 2 } }), expected)
-    assert.equal(util.stringify({ a: 2, x: { z: 1, a: 2 }, z: 1 }), expected)
+    assert.strict.equal(util.stringify({ z: 1, a: 2, x: { z: 1, a: 2 } }), expected)
+    assert.strict.equal(util.stringify({ a: 2, x: { z: 1, a: 2 }, z: 1 }), expected)
   })
 
   it('#hash()', () => {
-    assert.equal(util.hash({ z: 1, a: 2, x: { z: 1, a: 2 } }), '6be7b86bb9875d04833250dbd131c82f6209d773f15b01d86673e3157dd8c449')
-    assert.equal(util.hash({ x: { z: 1, a: 2 }, a: 2, z: 1 }), '6be7b86bb9875d04833250dbd131c82f6209d773f15b01d86673e3157dd8c449')
+    assert.strict.equal(util.hash({ z: 1, a: 2, x: { z: 1, a: 2 } }), '6be7b86bb9875d04833250dbd131c82f6209d773f15b01d86673e3157dd8c449')
+    assert.strict.equal(util.hash({ x: { z: 1, a: 2 }, a: 2, z: 1 }), '6be7b86bb9875d04833250dbd131c82f6209d773f15b01d86673e3157dd8c449')
   })
 
   it('#_optionsParser()', () => {
     const uri = 'http://www.example.com'
-    const expected = {uri, foo: true}
+    const expected = { uri, foo: true }
 
-    assert.deepEqual(util._optionsParser(uri, {foo: true}), expected)
-    assert.deepEqual(util._optionsParser({uri, foo: true}), expected)
+    assert.strict.deepEqual(util._optionsParser(uri, { foo: true }), expected)
+    assert.strict.deepEqual(util._optionsParser({ uri, foo: true }), expected)
   })
 
   it('#_normalizeUrl()', () => {
-    assert.equal(
+    assert.strict.equal(
       util._normalizeUrl('http://www.example.com:3000'),
       'http://www.example.com/'
     )
